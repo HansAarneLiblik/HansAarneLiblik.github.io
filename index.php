297373dd86eedec47cf8e8ae995f0f49f88cab07
Jaan Susi <jaan.susi@gmail.com>
2014-02-28 11:44:40 +0200

    lisasin search bar-i(mitte lõpuni), esimesed värvid, natuke css-i lehe jaoks (header,body)

--- index.php
+++ index.php
@@ -4,19 +4,30 @@
     <head>
         <meta charset="utf-8" />
         <title>Test site 101</title>
+        <link rel="stylesheet" type="text/css" href="css/desktop.css">
     </head>
     <body>
-        <h1>Testing Testing 123</h1>
+    	<div id="header">
+    		<a href="index.php" id="homeButton"><h1>Test site 101</h1></a>
+    		<input id="search" placeholder="Search" /><img id="searchButton" src="img/search-button.png" />
+    	</div>
+    	<div id="body">
+        
         <p>See on test site</p>
         <a href="lauri.html">Link lauri lehele</a>
-	<a href="jaan.html">Jaani leht</a>
-	<br />
-	<br />
-	<a href="Proto">Prototüübid</a>
-
-
-	<?php
-	echo 'see on php';
-	?>
+		<a href="jaan.html">Jaani leht</a>
+		<br />
+		<br />
+		<a href="Proto">Prototüübid</a>
+		
+		
+		<?php
+		echo 'see on php';
+		?>
+		</div>
+		<div id="login">
+			
+			
+		</div>
     </body>
 </html>
