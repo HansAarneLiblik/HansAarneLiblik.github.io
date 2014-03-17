f06fde36f688d2094c05690b74c36934b2c4cfdf
lauriuus <lauri.uusmaa@gmail.com>
2014-03-17 21:05:02 +0200

    eemaldasin "sisselogitud kasutaja" vaate

--- index.php
+++ index.php
@@ -1,87 +1,89 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/StyleSheet.css"/>
-		<title>Koodi Ämber</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	</head>
-	<body>
-		<table id="menu">
-			<tr>
-				<td id="logo" rowspan="2">
-					<div id="header" class="menuItem">
-						<a href="index.php">KoodiÄmber</a>
-					</div>
-				</td>
-				<td class="lingid" rowspan="2">
-					<div class="menuItem">
-						<a href="?p=amber">Minu ämber</a>
-					</div>
-				</td>
-				<td class="lingid" rowspan="2">
-					<div class="menuItem">
-						<a href="?p=profiil">Minu profiil</a>
-					</div>
-				</td>
-				<td class="lingid" rowspan="2">
-					<div class="menuItem">
-						<a href="?p=seaded">Seaded</a>
-					</div>
-				</td>
-				<td id="welcome">
-					<div class="menuItem">
-						  Tere &laquo; eesnimi &raquo; !
-					</div>
-				</td>
-				<td id="search">
-					<div class="menuItem">
-						<form  action="index.php" method="post">
-							<div>
-								<input type="text" id="searchBox" value=" Otsing..."/>
-								<input type="submit" value="Otsi"/>
-							</div>
-						</form>
-					</div>
-				</td>
-			</tr>
-			<tr>
-				<td></td>
-				<td id="logout">
-					<div class="menuItem">
-						<input type="button" value="Logi välja"/>
-					</div>
-				</td>
-			</tr>
-		</table>
-		
-		<p>
-		<br/>
-		<br/>
-		</p>
-		
-		<div id="content">
-			<?php
-			if (isset($_GET['p'])) {
-				switch ($_GET['p']) {
-					case "amber" :
-						include "minu_amber.html";
-						break;
-					case "profiil" :
-						include "minu_profiil.html";
-						break;
-					case "seaded" :
-						include "seaded.html";
-						break;
-					default :
-						include 'kodu.html';
-				}
-			} else {
-				include 'kodu.html';
-			}	
-			?>
-		</div>
-	</body>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/StyleSheet.css" />
+        <title>Koodi Ämber</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+        <table id="menu">
+            <tr>
+                <td id="logo" rowspan="2">
+                    <div id="header" class="menuItem">
+                        <a href="index.php">KoodiÄmber</a>
+                    </div>
+                </td>
+                <td rowspan="2">
+                    <div class="menuItem">
+                        
+                    </div>
+                </td>
+                <td rowspan="2">
+                    <div class="menuItem">
+                       
+                    </div>
+                </td>
+                <td rowspan="2">
+                    <div class="menuItem">
+                       
+                    </div>
+                </td>
+                <td id="welcome">
+                    <div class="menuItem">
+						  
+                    </div>
+                </td>
+                <td id="search">
+                    <div class="menuItem">
+                        <form action="index.php" method="post">
+                            <div>
+                                <input type="text" id="searchBox" value=" Otsing..." />
+                                <input type="submit" value="Otsi" />
+                            </div>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td id="logout">
+                    <div class="menuItem">
+                                                    <!--<form action="register.html" method="GET">
+                                                        <input type="submit" value="Logi välja" />-->
+                        <a class="lingid2" href="login.html">Logi sisse</a> või 
+                        <a class="lingid2" href="register.html">Registreeru</a>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+        </table>
+
+        <p>
+            <br />
+            <br />
+        </p>
+
+        <div id="content">
+            <?php
+                if (isset($_GET['p'])) {
+                    switch ($_GET['p']) {
+                        case "amber" :
+                            include "minu_amber.html";
+                            break;
+                        case "profiil" :
+                            include "minu_profiil.html";
+                            break;
+                        case "seaded" :
+                            include "seaded.html";
+                            break;
+                        default :
+                            include 'kodu.html';
+                    }
+                } else {
+                    include 'kodu.html';
+                }	
+            ?>
+        </div>
+    </body>
 </html>
