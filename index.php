985bba46b9f14a5101586d3e806c3422a2830c06
lauriuus <lauri.uusmaa@gmail.com>
2014-03-18 12:29:05 +0200

    html valiidsus

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <link rel="stylesheet" type="text/css" href="css/StyleSheet.css" />
         <title>Koodi Ämber</title>
@@ -16,22 +16,22 @@
                 </td>
                 <td rowspan="2">
                     <div class="menuItem">
-                        
+
                     </div>
                 </td>
                 <td rowspan="2">
                     <div class="menuItem">
-                       
+
                     </div>
                 </td>
                 <td rowspan="2">
                     <div class="menuItem">
-                       
+
                     </div>
                 </td>
                 <td id="welcome">
                     <div class="menuItem">
-						  
+
                     </div>
                 </td>
                 <td id="search">
@@ -50,10 +50,10 @@
                 <td id="logout">
                     <div class="menuItem">
                                                     <!--<form action="register.html" method="GET">
-                                                        <input type="submit" value="Logi välja" /></form>-->
-                        <a class="lingid2" href="login.html">Logi sisse</a> või 
+                                                                                                                                                                        <input type="submit" value="Logi välja" /></form>-->
+                        <a class="lingid2" href="login.html">Logi sisse</a> või
                         <a class="lingid2" href="register.html">Registreeru</a>
-                        
+
                     </div>
                 </td>
             </tr>
@@ -66,23 +66,26 @@
 
         <div id="content">
             <?php
-                if (isset($_GET['p'])) {
-                    switch ($_GET['p']) {
-                        case "amber" :
-                            include "minu_amber.html";
-                            break;
-                        case "profiil" :
-                            include "minu_profiil.html";
-                            break;
-                        case "seaded" :
-                            include "seaded.html";
-                            break;
-                        default :
-                            include 'kodu.html';
-                    }
-                } else {
-                    include 'kodu.html';
-                }	
+                   if (isset($_GET['reg'])) {
+                     echo "Registreerimine õnnestus! Proovi sisse logida!";
+                }
+                   if (isset($_GET['p'])) {
+                       switch ($_GET['p']) {
+                           case "amber" :
+                               include "minu_amber.html";
+                               break;
+                           case "profiil" :
+                               include "minu_profiil.html";
+                               break;
+                           case "seaded" :
+                               include "seaded.html";
+                               break;
+                           default :
+                               include 'kodu.html';
+                       }
+                   } else {
+                       include 'kodu.html';
+                   }	
             ?>
         </div>
     </body>
