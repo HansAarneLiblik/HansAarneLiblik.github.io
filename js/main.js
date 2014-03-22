f6ea9dac4aff4c87458f202121f2fe0459270e82
lauriuus <lauri.uusmaa@gmail.com>
2014-03-23 01:40:55 +0200

    AJAX vist

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    if (document.location.hash == "" || document.location.hash == "#") {
+        $("#content").load("kodu.html");
+    } else {
+        $("#content").load(document.location.hash.substr(1)+".html");
+    }
+    $("#header").click(function () {
+        $("#content").load("kodu.html");
+    });
+    $("#loginLink").click(function () {
+        $("#content").load("login.html");
+    });
+    $("#registerLink").click(function () {
+        $("#content").load("register.html");
+    });
+});
