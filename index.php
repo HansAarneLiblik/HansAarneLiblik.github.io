fe2c4a749eb4e7cd9f45628f0aeae36c23ea17f4
lauriuus <lauri.uusmaa@gmail.com>
2014-03-29 23:59:33 +0200

    valiidsuse katse

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <?php
     session_start();
 ?>
