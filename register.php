fbbeb2a6b4efb6fad9d0ea9d9662c108f2de3eb2
lauriuus <lauri.uusmaa@gmail.com>
2014-03-29 23:24:18 +0200

    code cleanup

--- register.php
+++ register.php
@@ -2,20 +2,12 @@
     
     $username = $_POST['username'];
     $password = $_POST['password'];
-    //$firstname = $_POST['firstname'];
-    //$lastname = $_POST['lastname'];
     $email = $_POST['email'];
-    //$bday = $_POST['bday'];
-    
-    //echo $username, "<br>", $password, "<br>", $firstname, "<br>", $lastname, "<br>", $email;
-        
     $ctx = hash_init('md5');
     hash_update($ctx, $password);
     $hashed_pw = (string)hash_final($ctx);
 
     $dbconn = pg_connect("host=vrl.liblik.ee port=5432 dbname=veebirak user=postgres password=lollakas");
-    //$dbconn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=vrl4ever");
-    //$query = "insert into users (username, password, firstname, lastname, email, date_of_birth) VALUES ('$username', '$hashed_pw', '$firstname', '$lastname', '$email', '$bday')";
     $query = "insert into users2 (username, password, email) VALUES ('$username', '$hashed_pw', '$email')";
 
     $result = pg_query($query) or die('Query failed: ' . pg_last_error());
