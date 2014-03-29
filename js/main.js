ef056ffe8ac13589556dbae2e9cd7901699c3a94
lauriuus <lauri.uusmaa@gmail.com>
2014-03-29 23:37:03 +0200

    lisatud #minu_amber, #seaded, #profiil

--- js/main.js
+++ js/main.js
@@ -13,4 +13,13 @@ $(document).ready(function () {
     $("#registerLink").click(function () {
         $("#content").load("register.html");
     });
+    $("#minu_amber").click(function () {
+        $("#content").load("minu_amber.html");
+    });
+    $("#profiil").click(function () {
+        $("#content").load("minu_profiil.html");
+    });
+    $("#seaded").click(function () {
+        $("#content").load("seaded.html");
+    });
 });
