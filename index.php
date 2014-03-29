c4bec0e71dd93132ee13824bd15f098d9f86bff5
lauriuus <lauri.uusmaa@gmail.com>
2014-03-29 23:24:07 +0200

    sisselogimisel näeb menüüs uusi valikuid

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <?php
-session_start();
+    session_start();
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -13,13 +12,19 @@ session_start();
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
         <title>Koodi Ämber</title>
         <?php
-			if (isset($_SESSION['user'])) {
-				$welcome = "Tere, ".$_SESSION['user']."!";
-			} else {
-				$welcome = "Pole sisse logitud.";	
-			}
-			
-		?>
+            if (isset($_SESSION['user'])) {
+                $welcome = "Tere, ".$_SESSION['user']."!";
+                $menu = "<ul>
+                            <li><a href=\"/#minu_amber\" title=\"Minu Ämber\">Minu Ämber</a></li>
+                            <li><a href=\"/#profiil\" title=\"Profiil\">Profiil</a></li>
+                            <li id=\"seaded\"><a href=\"/#seaded\" title=\"Seaded\">Seaded</a></li>
+                        </ul>";	
+            } else {
+                $welcome = "Pole sisse logitud.";
+                $menu = "";                
+            }
+            
+        ?>
     </head>
     <body>
         <table id="menu">
@@ -31,24 +36,17 @@ session_start();
                 </td>
                 <td rowspan="2">
                     <div class="menuItem">
-						<?php
-							echo $welcome;
-						?>
+                        <?php
+                            echo $menu;
+                        ?>
                     </div>
                 </td>
-                <td rowspan="2">
-                    <div class="menuItem">
 
-                    </div>
-                </td>
-                <td rowspan="2">
-                    <div class="menuItem">
-
-                    </div>
-                </td>
                 <td id="welcome">
                     <div class="menuItem">
-
+                        <?php
+                            echo $welcome;
+                        ?>
                     </div>
                 </td>
                 <td id="search">
@@ -64,38 +62,38 @@ session_start();
             </tr>
             <tr>
                 <td></td>
-                
+
                 <td id="logout">
-                	<?php
-					if (!isset($_SESSION['user'])) {
-						echo "
-							<div class=\"menuItem\">
-								<a id=\"loginLink\" class=\"lingid2\" href=\"#login\">Logi sisse</a> või
-								<a id=\"registerLink\" class=\"lingid2\" href=\"#register\">Registreeru</a>
-							</div>
-						";
-					} else {
-						echo "
-							<div class=\"menuItem\">
-								<a id=\"logoutLink\" class=\"lingid2\" href=\"logout.php\">Logi välja</a>
-							</div>
-						";
-					}
-					?>
+                    <?php
+                        if (!isset($_SESSION['user'])) {
+                            echo "
+                                <div class=\"menuItem\">
+                                    <a id=\"loginLink\" class=\"lingid2\" href=\"#login\">Logi sisse</a> või
+                                    <a id=\"registerLink\" class=\"lingid2\" href=\"#register\">Registreeru</a>
+                                </div>
+                            ";
+                        } else {
+                            echo "
+                                <div class=\"menuItem\">
+                                    <a id=\"logoutLink\" class=\"lingid2\" href=\"logout.php\">Logi välja</a>
+                                </div>
+                            ";
+                        }
+                    ?>
                 </td>
-				
+
             </tr>
         </table>
 
 
         <div id="content">
-        	<?php
-				if (isset($_SESSION['notice'])) {
-					echo $_SESSION['notice'];
-					unset($_SESSION['notice']);
-				}
-			?>
-            
+            <?php
+                if (isset($_SESSION['notice'])) {
+                    echo $_SESSION['notice'];
+                    unset($_SESSION['notice']);
+                }
+            ?>
+
         </div>
     </body>
 </html>
