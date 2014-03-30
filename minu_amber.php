b7f27d2c74fec10811f2038cb1d92cc9d66d4e5c
HansAarneLiblik <hans.aarne@liblik.ee>
2014-03-30 22:18:22 +0300

    Uued koodid parandused, minu_amber tabeliks, css parandused

--- minu_amber.php
+++ minu_amber.php
@@ -5,23 +5,25 @@
 <table class="pasteTable">
 	<tbody>
 		<tr class="tableHead">
+			<th scope="col">Nr.</th>
+			<th scope="col">ID</th>
 			<th scope="col">Nimi</th>
 			<th scope="col">Lisatud</th>
-			<th scope="col">Aegub</th>
 		</tr>
-		<tr>
-			<?php
-            	if (isset($_SESSION['id'])) {
-                    $id = $_SESSION['id'];
-                    $dbconn = pg_connect("host=vrl.liblik.ee port=5432 dbname=veebirak user=postgres password=lollakas");
-                    $query = "SELECT id FROM codes WHERE author_id='$id'";
-                    $result = pg_query($query);
-                    while ($row = pg_fetch_row($result)) {
-						echo "Id: $row[0]";
-						echo "<br />\n";
-					}
-                }
-            ?>
-		</tr>
+		<?php
+			if (isset($_SESSION['id'])) {
+				$id = $_SESSION['id'];
+				$dbconn = pg_connect("host=vrl.liblik.ee port=5432 dbname=veebirak user=postgres password=lollakas");
+				$query = "SELECT * FROM codes WHERE author_id='$id'";
+				$result = pg_query($query);
+				$i = 1;
+				while ($val = pg_fetch_row($result)) {
+					echo "<tr><td>$i. </td><td>$val[0]</td><td><a href=\"/vaade.php?id=$val[0]\">$val[1]</a></td>
+					<td>$val[4]</td></tr>";
+					$i = $i + 1;
+				}
+				pg_close($dbconn);
+			}
+		?>
 	</tbody>
 </table>
