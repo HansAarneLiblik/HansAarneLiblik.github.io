005f299e8ab9a1db9e298c247349cf38b818c426
HansAarneLiblik <hans.aarne@liblik.ee>
2014-03-30 21:48:21 +0300

    Uued koodid bug fix

--- index.php
+++ index.php
@@ -91,17 +91,20 @@
             <div class="left-content left">
             </div>
             <div class="right-content right">
-				<?php
-					$dbconn = pg_connect("host=vrl.liblik.ee port=5432 dbname=veebirak user=postgres password=lollakas");
-					$query = "SELECT * FROM codes WHERE privacy=FALSE ORDER BY time_stamp DESC LIMIT 10";
-					$result = pg_query($query);
-					$i = 1;
-					while($val = pg_fetch_row($result)){
-						echo "<tr><td>".$i.". <a href=\"/vaade.php?id=".$val[0]."\">".$val[1]."</a></td></tr>";
-						$i = $i + 1;
-					}
-					pg_close($dbconn);
-				?>
+				<h3>Uued koodid</h3>
+				<table>
+					<?php
+						$dbconn = pg_connect("host=vrl.liblik.ee port=5432 dbname=veebirak user=postgres password=lollakas");
+						$query = "SELECT * FROM codes WHERE privacy=FALSE ORDER BY time_stamp DESC LIMIT 10";
+						$result = pg_query($query);
+						$i = 1;
+						while($val = pg_fetch_row($result)){
+							echo "<tr><td>".$i.". <a href=\"/vaade.php?id=".$val[0]."\">".$val[1]."</a></td></tr>";
+							$i = $i + 1;
+						}
+						pg_close($dbconn);
+					?>
+				</table>
             </div>
         </div>
     </body>
