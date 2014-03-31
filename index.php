bcabffdf45529825902b28faf3ec3703bb0f4458
lauriuus <lauri.uusmaa@gmail.com>
2014-04-01 02:16:46 +0300

    HTML valiidsus

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     session_start();
 ?>
 <?xml version="1.0" encoding="UTF-8" ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
-    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 
 
 <html xmlns="http://www.w3.org/1999/xhtml">
