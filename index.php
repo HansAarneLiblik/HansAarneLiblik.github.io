2822e104a2264a28d59342ce0955b4dabf726fbf
Hans Aarne Liblik <hans.aarne@liblik.ee>
2014-04-04 11:25:46 +0300

    Join lause ja autor uutel koodidel

--- index.php
+++ index.php
@@ -92,14 +92,21 @@
             </div>
             <div class="right-content right">
 				<h3>Uued koodid</h3>
-				<table>
+				<table class="newCodesTable">
 					<?php
 						$dbconn = pg_connect("host=vrl.liblik.ee port=5432 dbname=veebirak user=postgres password=lollakas");
-						$query = "SELECT * FROM codes WHERE privacy=FALSE ORDER BY time_stamp DESC LIMIT 10";
+						$query = "SELECT codes.id, codes.name, users2.firstname, users2.username FROM codes INNER JOIN users2 ON codes.author_id=users2.id WHERE codes.privacy=FALSE ORDER BY codes.time_stamp DESC LIMIT 10";
 						$result = pg_query($query);
 						$i = 1;
 						while($val = pg_fetch_row($result)){
 							echo "<tr><td>$i. </td><td><a href=\"#vaade.php?id=$val[0]\" class=\"uuedKoodid\">$val[1]</a></td></tr>";
+							echo "<tr>";
+							if($val[2]==""){
+								echo "<td></td><td class=\"authorName\">- $val[3]</td>";
+							} else {
+								echo "<td></td><td class=\"authorName\">- $val[2]</td>";
+							}
+							echo "</tr>";
 							$i = $i + 1;
 						}
 						pg_close($dbconn);
